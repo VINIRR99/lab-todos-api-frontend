a68ab4942065c799e4831dc72b408dfd09798915
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-12 17:40:36 -0400

    inactive form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 const Form = () => {
-    return <>Form</>
+    const [todo, setTodo] = useState("");
+
+    return (
+        <form>
+            <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
+            <button>Adicionar</button>
+        </form>
+    );
 };
 
 export default Form;
