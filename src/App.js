c5e3f487c477e38f6e242fede9fbd5d26341d1d6
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-12 17:33:34 -0400

    create Form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Title from "./components/title";
+import Form from "./components/form";
 
 const App = () => {
   return <>
     <Title />
+    <section style={{margin: "0 15px"}}>
+      <Form />
+    </section>
   </>;
 }
 
