baeec8a621d64f801cd3c5f5e641e02281c22de4
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-13 16:15:13 -0400

    initial list

--- src/components/todos-list/index.js
+++ src/components/todos-list/index.js
@@ -1,36 +1,10 @@
 import { useState, useEffect } from "react";
 import { todosApi } from "../../utils/todosApi";
 
-const list = [
-    {
-        _id: "622d622ec10973fb454ed8f5",
-        title: "Do the laundry",
-        completed: true
-    },
-    {
-        _id: "622d6230c10973fb454ed8f7",
-        title: "Do the frontend",
-        completed: false
-    },
-    {
-        _id: "622d6284c10973fb454ed8fc",
-        title: "Do the dishes",
-        completed: true
-    }
-];
-
 const TodosList = () => {
     const [todos, setTodos] = useState([]);
 
-    const [test, setTest] = useState([]);
-
-    useEffect(() => (async () => {
-        setTodos(list);
-
-        setTest(await todosApi.getTodos());
-    })(), []);
-
-    console.log("Test", test);
+    useEffect(() => (async () => setTodos(await todosApi.getTodos()))(), []);
 
     return (
         <div>
--- src/utils/todosApi.js
+++ src/utils/todosApi.js
@@ -3,13 +3,6 @@ import axios from "axios";
 class TodosApi {
     constructor () {
         this.api = axios.create({ baseURL: `${process.env.REACT_APP_TODOS_KEY}/todo` });
-        this.testAxios = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1/1" });
-    };
-    test = async () => {
-        try {
-            const { data: { meals } } = await this.testAxios.get("/filter.php?c=Seafood");
-            return meals;
-        } catch (error) {throw new Error(`Cannot get Recipes => ${error}`)};
     };
     getTodos = async () => {
         try {
