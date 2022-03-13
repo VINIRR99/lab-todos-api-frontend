20c2bdd26f09b9af1897f60112c21bd0847a744f
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-13 16:20:20 -0400

    puto the list in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import { todosApi } from "./utils/todosApi";
 import Title from "./components/title";
 import AddTodo from "./components/form";
 import TodosList from "./components/todos-list";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => (async () => setTodos(await todosApi.getTodos()))(), []);
+
   return <>
     <Title />
     <section style={{margin: "0 15px"}}>
       <AddTodo />
-      <TodosList />
+      <TodosList todos={todos} />
     </section>
   </>;
 }
--- src/components/todos-list/index.js
+++ src/components/todos-list/index.js
@@ -1,11 +1,4 @@
-import { useState, useEffect } from "react";
-import { todosApi } from "../../utils/todosApi";
-
-const TodosList = () => {
-    const [todos, setTodos] = useState([]);
-
-    useEffect(() => (async () => setTodos(await todosApi.getTodos()))(), []);
-
+const TodosList = ({ todos }) => {
     return (
         <div>
             {todos.map(todo => <h2 key={todo._id}>{todo.title}</h2>)}
