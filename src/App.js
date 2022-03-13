32b6e24a14db25f118505f362012034ca8796e49
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-13 14:29:35 -0400

    create TodosList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Title from "./components/title";
 import AddTodo from "./components/form";
+import TodosList from "./components/todos-list";
 
 const App = () => {
   return <>
     <Title />
     <section style={{margin: "0 15px"}}>
       <AddTodo />
+      <TodosList />
     </section>
   </>;
 }
---
+++ src/components/todos-list/index.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const list = [
+    {
+        _id: "622d622ec10973fb454ed8f5",
+        title: "Do the laundry",
+        completed: true
+    },
+    {
+        _id: "622d6230c10973fb454ed8f7",
+        title: "Do the frontend",
+        completed: false
+    },
+    {
+        _id: "622d6284c10973fb454ed8fc",
+        title: "Do the dishes",
+        completed: true
+    }
+];
+
+const TodosList = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => (async () => setTodos(list))(), []);
+
+    return (
+        <div>
+            {todos.map(todo => <h2 key={todo._id}>{todo.title}</h2>)}
+        </div>
+    );
+};
+
+export default TodosList;
