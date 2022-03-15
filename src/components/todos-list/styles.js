c91eeef0901cf228b9c86f2acbe70996a4c58407
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-14 21:50:02 -0400

    list styles done

---
+++ src/components/todos-list/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+    background-color: #f5f5f5;
+    height: 40px;
+    border-bottom: 1px solid rgba(128, 128, 128, 0.5);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    span {
+        text-decoration: ${({ completed }) => completed && "line-through"};
+    }
+`;
+
+export const Button = styled.button`
+    background-color: #e54f2d;
+    width: 20px;
+    height: 20px;
+    color: white;
+    font-size: 10px;
+    border: 0;
+    border-radius: 50%;
+    cursor: pointer;
+`;
